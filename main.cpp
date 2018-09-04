54d502bd1f7cdc89b1a20c5d8e820a843ba5d3ec
Justyna Gorczyca <just.gorcz@gmail.com>
2018-09-04 08:28:20 +0200

    vector

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <array>
+#include <vector>
 using namespace std;
 
 
@@ -32,11 +33,60 @@ void arrayClass()
     int front = myArray.front();
     cout << ".front() " << front << endl; //front returns reference 
     cout << ".back() " << myArray.back() << endl;
+
+    //------------------------------------------------------------------
+
+    //const char* cstr = "Test string";
+    //cout << "cstr.size() " << cstr.size() << endl; //neee
+}
+
+void vectorClass()
+{
+    vector<int> vect;
+    for(int i = 0; i < 5; i++) vect.push_back(i);
+
+    vect.resize(2);
+    vect.resize(8,100);
+    vect.resize(12);
+
+    for(vector<int>::iterator it = vect.begin(); it != vect.end(); ++it)
+        cout << " " << *it;
+    cout << endl;
+
+    cout << ".capacity() " << vect.capacity() << endl;
+    cout << ".size() " << vect.size() << endl;
+    cout << ".max_size() " << vect.max_size() << endl;
+
+    vect.shrink_to_fit();
+    cout << ".capacity() after .shrink_to_fit() " << vect.capacity() << endl;
+
+    //------------------------------------------------------
+    vector<int> vect2;
+    vect2.assign(7,100);
+
+    vector<int>::iterator it;
+    it = vect2.begin() + 1;
+
+    vector<int> vect3;
+    vect3.assign(it, vect2.end() - 1); 
+
+    for(vector<int>::iterator it2 = vect2.begin(); it2 != vect2.end(); ++it2)
+        cout << " " << *it2;
+    cout << endl;
+
+    it = vect3.begin();
+    it = vect3.insert(it, 234);
+
+    for(vector<int>::iterator it3 = vect3.begin(); it3 != vect3.end(); ++it3)
+        cout << " " << *it3;
+    cout << endl;
+
 }
 
 int main()
 {
     cout << "Hello again!\n";
     arrayClass();
+    vectorClass();
     return 0;
 }
