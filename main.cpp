5668f8cdc38eb1ecb991ff47b7d0ccb4bb7c5ccd
Justyna Gorczyca <just.gorcz@gmail.com>
2018-09-04 21:44:18 +0200

    deque

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <deque>
 using namespace std;
 
 
@@ -33,11 +34,6 @@ void arrayClass()
     int front = myArray.front();
     cout << ".front() " << front << endl; //front returns reference 
     cout << ".back() " << myArray.back() << endl;
-
-    //------------------------------------------------------------------
-
-    //const char* cstr = "Test string";
-    //cout << "cstr.size() " << cstr.size() << endl; //neee
 }
 
 void vectorClass()
@@ -77,9 +73,53 @@ void vectorClass()
     it = vect3.begin();
     it = vect3.insert(it, 234);
 
+    cout << ".insert(it, 243) ";
     for(vector<int>::iterator it3 = vect3.begin(); it3 != vect3.end(); ++it3)
         cout << " " << *it3;
     cout << endl;
+
+    vect3.erase(vect3.begin() + 1);
+    cout << ".erase(.begin() + 1) ";
+    for(vector<int>::iterator it3 = vect3.begin(); it3 != vect3.end(); ++it3)
+        cout << " " << *it3;
+    cout << endl;
+}
+
+/*
+ * deque (usually pronounced like "deck") is an irregular acronym of double-ended queue. Double-ended queues are sequence containers with dynamic sizes that can be expanded or contracted on both ends (either its front or its back).
+ * While vectors use a single array that needs to be occasionally reallocated for growth, the elements of a deque can be scattered in different chunks of storage, with the container keeping the necessary information internally to provide direct access to any of its elements in constant time and with a uniform sequential interface (through iterators).
+ */
+
+void dequeClass()
+{
+    int tab[] = { 1, 3, 5, 7};
+    deque<int> queue(tab, tab + sizeof(tab)/sizeof(int));
+
+    deque<int>::iterator it = queue.begin();
+    while(it != queue.end())
+        cout << *it++ << " ";
+    cout << endl;
+
+    cout << "dque.size() " << queue.size() << endl;
+    queue.push_front(1234);
+
+    cout << "dque.push_front(1234) ";
+    it = queue.begin();
+    while(it != queue.end())
+        cout << *it++ << " ";
+    cout << endl;
+
+    //epmlace returns: An iterator that points to the newly emplaced element.
+    auto it2 = queue.emplace(queue.begin() + 1, 77);
+    queue.emplace(it2, 88);
+    queue.emplace(queue.end(), 99);
+
+    cout << "dque.enplace() ";
+    it = queue.begin();
+    while(it != queue.end())
+        cout << *it++ << " ";
+    cout << endl;
+
 
 }
 
@@ -88,5 +128,6 @@ int main()
     cout << "Hello again!\n";
     arrayClass();
     vectorClass();
+    dequeClass();
     return 0;
 }
